fa70258fbae65c7054e2329bb406c48c1a7b58e7
AlexKanotz <96555799+AlexKanotz@users.noreply.github.com>
2022-12-29 13:27:36 -0500

    Update startMenu.java

--- startMenu.java
+++ startMenu.java
@@ -8,7 +8,7 @@ import java.awt.*;
 // **********************************************************
 public class startMenu implements ActionListener {
     JFrame frame;
-    JLabel label;
+    JLabel label, label1;
     JButton startButton, howToPlayButton, exitButton;
 
     // **********************************************************
@@ -19,24 +19,32 @@ public class startMenu implements ActionListener {
         label = new JLabel("Checkers");
         label.setBounds(175, 50, 150, 25); // x, y, width, height
         label.setFont(new Font("Comic Sans MS", Font.PLAIN, 30));
+        
+        // Add label
+        label1 = new JLabel("Created by: Alex Kanotz");
+        label1.setBounds(135, 150, 300, 25); // x, y, width, height
+        label1.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
 
         // Add start button
-        startButton = new JButton("START");
+        startButton = new JButton("Start");
         startButton.setBounds(180, 250, 120, 30);
         startButton.addActionListener(this);
         startButton.setFocusable(false);
+        startButton.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
 
         // Add how to play button
-        howToPlayButton = new JButton("HOW TO PLAY");
+        howToPlayButton = new JButton("How To Play");
         howToPlayButton.setBounds(180, 285, 120, 30);
         howToPlayButton.addActionListener(this);
         howToPlayButton.setFocusable(false);
+        howToPlayButton.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
 
         // Add exit button
-        exitButton = new JButton("EXIT");
+        exitButton = new JButton("Exit");
         exitButton.setBounds(180, 320, 120, 30);
         exitButton.addActionListener(this);
         exitButton.setFocusable(false);
+        exitButton.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
 
         // Display start menu GUI
         frame = new JFrame();
@@ -49,6 +57,7 @@ public class startMenu implements ActionListener {
         frame.getContentPane().setBackground(new Color(118, 142, 181));
         frame.setResizable(false);
         frame.add(label);
+        frame.add(label1);
         frame.add(startButton);
         frame.add(howToPlayButton);
         frame.add(exitButton);
